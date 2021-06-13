69ab401c6f8cd88e62f32a6abd72d417f66a0fa8
anandkumarcoder <akanand104@gmail.com>
2021-06-13 17:11:42 +0530

    import JS

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Script is working from diffferent file")
+
+//input
+var username = prompt("Enter your name below")
+
+// processing
+var welcomeMessage = "This script works " + username;
+
+// output
+alert(welcomeMessage)
